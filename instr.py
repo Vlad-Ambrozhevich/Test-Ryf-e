30e8866f7ac2b6989cf5a3783a37bd5484abf01d
Vlad-Ambrozhevich <132078523+Vlad-Ambrozhevich@users.noreply.github.com>
2023-04-29 19:55:03 +0300

    Create instr.py

---
+++ instr.py
@@ -0,0 +1,30 @@
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = ''
+txt_title = 'Здоровье'
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+txt_next = ''
+txt_instruction = 'Забота о здоровье ― это важная часть жизни большинства людей. Не у всех есть возможность регулярно проходить профессиональные медицинские осмотры. Однако существует ряд несложных тестов, с помощью которых можно достаточно точно оценить состояние своего здоровья. Один из таких тестов — тест Руфье, который позволяет определить состояние кардиореспираторной системы организма. ' 
+txt_button = 'Начать'
+txt_title = ''
+txt_name = ''
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = ''
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''
